07d147481212f2982ae5f41f1e9f689b14864e8b
Rich Cahill <rich@rc3.me>
2022-12-12 13:47:00 +0000

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import '../public/fonts/redaction/redaction.css';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
